0871ce7aa11afa6e14bdfea301c4a5c0fec9d4ed
Dominic Ginger <domginger@hotmail.co.uk>
2017-03-10 18:06:50 +0000

    Init commit

---
+++ lib/is_it_down.ex
@@ -0,0 +1,18 @@
+defmodule IsItDown do
+  @moduledoc """
+  Documentation for IsItDown.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> IsItDown.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/is_it_down_test.exs
@@ -0,0 +1,8 @@
+defmodule IsItDownTest do
+  use ExUnit.Case
+  doctest IsItDown
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
